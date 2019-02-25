b59c94be05f23755199bd462aa8c9d5d2cb9c877
Myles Bennett <myles.bennett@hotmail.com>
2019-02-25 11:11:52 -0500

    added comments and reciept format for print output

--- product.py
+++ product.py
@@ -1,11 +1,13 @@
 class Product:
     """ Calculates product total price based on base price and tax rate """
 
+    # initializes a product with name, price, and tax rate
     def __init__(self, name, base_price, tax_rate):
         self.name = name
         self.base_price = base_price
         self.tax_rate = tax_rate
 
+    # calculates the total price of a product including tax
     def total_price(self):
         total = self.base_price + (self.base_price * (self.tax_rate / 100))
         return total
--- shopping_cart.py
+++ shopping_cart.py
@@ -8,18 +8,23 @@ class ShoppingCart:
     def __init__(self):
         self.cart = []
 
+    # method to add a product to the cart
     def add_product(self, product):
         self.cart.append(product)
 
+    # method to remove a product to the cart
     def remove_product(self, product):
         self.cart.remove(product)
 
+    # calculates the total price of products in cart before tax
     def total_before_tax(self):
         subtotal = 0
         for item in self.cart:
             subtotal += item.base_price
         return subtotal
 
+    # calculates the total price of items in the cart using the total price
+    # method from Product class
     def total_after_tax(self):
         total = 0
         for item in self.cart:
@@ -27,6 +32,7 @@ class ShoppingCart:
         return total
 
 
+# Testing
 apple = Product('apple', 2, 13)
 shirt = Product('shirt', 25, 13)
 beer = Product('banana', 15, 15)
@@ -35,8 +41,13 @@ my_cart = ShoppingCart()
 my_cart.add_product(apple)
 my_cart.add_product(shirt)
 my_cart.add_product(beer)
-print(my_cart.cart)
-print(my_cart.total_before_tax())
 my_cart.remove_product(apple)
-print(my_cart.cart)
-print(my_cart.total_after_tax())
+print('------------------')
+for item in my_cart.cart:
+    print("{}: ${:.2f}".format(item.name, item.base_price))
+
+print('------------------')
+print("Subtotal: ${:.2f}".format(my_cart.total_before_tax()))
+print('------------------')
+print("Total: ${:.2f}".format(my_cart.total_after_tax()))
+print('------------------')
